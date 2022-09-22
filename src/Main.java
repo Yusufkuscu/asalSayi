386e452e51c83b3b630cd52688b81c9901be6017
YUSUF KUSCU <kyusuf40ky@gmail.com>
2022-09-23 02:28:51 +0300

    asal sayi bulundu

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isPrime;
+        for (int i = 2; i <= 100; i++) {
+            isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i + " ");
+            }
+        }
+
+    }
+}
